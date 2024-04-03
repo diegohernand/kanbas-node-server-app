99e14ffb34d283d88f11d92ffc89dc2edbfc36ed
diegohernand <diggs.a.hedz@gmail.com>
2024-04-03 18:55:49 -0400

    fixed import bug

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import express from "express";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import cors from "cors";
-import ModuleRoutes from "./Kanbas/modules/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import Hello from "./Hello.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 app.listen(process.env.PORT || 4000);
