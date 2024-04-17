cf97bf1bc7b4d3cd42cecca3837e02a758045fe8
diegohernand <diggs.a.hedz@gmail.com>
2024-04-17 15:32:40 -0400

    kind of working

--- App.js
+++ App.js
@@ -3,9 +3,23 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import cors from "cors";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import Hello from "./Hello.js";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+import dotenv from "dotenv";
+
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING 
+
+mongoose.connect("mongodb+srv://giuseppi:supersecretpassword@cluster0.hiyo5yg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+    })
+   );
 app.use(express.json());
+UserRoutes(app);
 Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
